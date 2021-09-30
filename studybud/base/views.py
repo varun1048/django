e1174eace7b407f296fdafc0ec3f121c24626d29
varun <59528216+varun1048@users.noreply.github.com>
2021-09-30 21:42:26 +0530

    django

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Room
+from .forms import RoomForm
 
 def home(req):
     context = Room.objects.all()
@@ -9,5 +10,27 @@ def room(req,pk):
 
     context =  Room.objects.get(id=pk)
     return render(req,'base/room.html',{"room":context})
+
+def createRoom(req):
+    context= {'form':RoomForm()}
+    if req.method == 'POST':
+        form = RoomForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(req,'base/room_form.html',context)
+
+def updateRoom(req,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if req.method == 'POST':
+        form = RoomForm(req.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context= {'form': form}
+    return render(req,'base/room_form.html',context)
 
 
