0e9be38907e0e6ce7692a064cc36b58c9b3de9aa
varun <59528216+varun1048@users.noreply.github.com>
2021-10-24 18:02:11 +0530

    base
    all basement done for members and enquirys

--- Spartanz/administrator/views.py
+++ Spartanz/administrator/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from .models import Enquirys,Member
 # Create your views here.
 def home(req):
-    return render(req,'administrator/home.html')
+
+    return render(req,'administrator/home.html',{
+        "enquirys":len(Enquirys.objects.all()),
+        "members":len(Member.objects.all()),
+        "attendance":None,
+        })
 
 def attendance(req):
     return render(req,'administrator/attendance.html')
